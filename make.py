e41baac533c4d23b8b52c471caf869b1e1b620ef
Hugo Ledoux <h.ledoux@tudelft.nl>
2014-07-24 13:20:25 +0200

    add google analytics tracker

--- make.py
+++ make.py
@@ -35,6 +35,7 @@ def main():
 
   print "done."
 
+
 def write_pubs():
   s = StringIO.StringIO()
   s1 = """
@@ -83,9 +84,17 @@ def write_footer():
   ================================================== -->
   <!-- Placed at the end of the document so the pages load faster -->
   <script src="assets/js/classie.js"></script>
-    <script src="assets/js/bootstrap.min.js"></script>
-    <script src="assets/js/smoothscroll.js"></script>
+  <script src="assets/js/bootstrap.min.js"></script>
+  <script src="assets/js/smoothscroll.js"></script>
   <script src="assets/js/main.js"></script>
+  <script>
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+    ga('create', 'UA-53152577-1', 'auto');
+    ga('send', 'pageview');
+  </script>
 </body>
 </html>
   """
@@ -110,8 +119,7 @@ def write_header():
     <link href="assets/css/main.css" rel="stylesheet">
     <link href="http://maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet">
 
-    <script src="assets/js/jquery.min.js"></script>
-  
+    <script src="http://code.jquery.com/jquery-1.11.0.min.js"></script>  
 
     <link href='http://fonts.googleapis.com/css?family=Ubuntu' rel='stylesheet' type='text/css'>
     <link href='http://fonts.googleapis.com/css?family=Ubuntu+Condensed' rel='stylesheet' type='text/css'>
