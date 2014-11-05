fba9807298360ed63f736ea6a3dcf06c0f553bd1
Hugo Ledoux <h.ledoux@tudelft.nl>
2014-11-05 12:42:06 +0100

    updated to 3dgeoinfo!

--- go.php
+++ go.php
@@ -2,4 +2,4 @@
 	include('/Users/hugo/www/bibhtmler/bibhtmler.php');
 	$bibhtmler = new bibhtmler(array('groupby' => 'year'));
 ?>
-<?php echo($bibhtmler->process('/Users/hugo/Dropbox/references/hugomyown.bib')) ?>
+<?php echo($bibhtmler->process('/Users/hugo/references/hugomyown.bib')) ?>
