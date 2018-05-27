e715edd3a0ae3645f1cb4a5df5a67768f2b2b447
Hugo Ledoux <h.ledoux@tudelft.nl>
2018-05-27 16:09:31 +0200

    python3 + new header with open access icon

--- _pubs/buildpubs.py
+++ _pubs/buildpubs.py
@@ -1,23 +1,23 @@
 import os
-import StringIO
+from io import StringIO
 import datetime
 
 
 def write_pubs():
-  s = StringIO.StringIO()
+  s = StringIO()
   header = "---\nlayout: page\ntitle: publications\npermalink: /pubs/\n---\n\n"
-  print >>s, header
+  print (header, file=s)
   update = '<h1>Publications</h1>\n<span class="post-date">(last update: %s)</span>' % (datetime.date.today().isoformat())
-  print >>s, update
+  print (update, file=s)
   toc = gettoc()
   warning = getwarning()
-  print >>s, warning
-  print >>s, toc
-  print >>s, "\n{% raw %}"
+  print (warning, file=s)
+  print (toc, file=s)
+  print ("\n{% raw %}", file=s)
 
   f = open('pubs.html')
-  print >>s, f.read()
-  print >>s, '{% endraw %}'
+  print (f.read(), file=s)
+  print ('{% endraw %}', file=s)
   os.remove('pubs.html')
   return s 
 
@@ -26,9 +26,15 @@ def getwarning():
     s = """<div class="message">
       I provide here the author's version of most of my papers. 
       These are for <em>personal use only</em>, and not for redistribution or commercial use. 
-      Please use the official published version ( <i class="fa fa-bookmark"></i> ) if you have access to it.
+      Please use the official published version ( <a><i class="fa fa-bookmark"></i></a> ) if you have access to it.
       <br><br>
-      Where applicable, a link to the "reproducibility repository" ( <i class="fa fa-github"></i> ), where the code and/or the data needed to reproduce the results of the paper, is given.
+      <a><i class="ai ai-open-access-square"></i></a> = open access
+      <br><br>
+      <a><i class="fa fa-github"></i></a> = the "reproducibility repository", where the code and/or the data needed to reproduce the results of the paper, is given.
+      <br><br>
+      <i class="fa fa-external-link"></i> <a href='https://orcid.org/0000-0002-1251-8654'> my ORCID page</a>
+      <br>
+      <i class="fa fa-external-link"></i> <a href='http://www.scopus.com/inward/authorDetails.url?authorID=23988925500&partnerID=MN8TOARS'> my SCOPUS page</a>
       </div>
     """
     return s
