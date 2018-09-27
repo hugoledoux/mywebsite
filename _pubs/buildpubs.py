80e92d69a43ac7a2d94bc9425ea5d1c339898f48
Hugo Ledoux <h.ledoux@tudelft.nl>
2018-09-27 09:06:51 +0200

    Revert "python3 + new header with open access icon"
    This reverts commit e715edd3a0ae3645f1cb4a5df5a67768f2b2b447.

--- _pubs/buildpubs.py
+++ _pubs/buildpubs.py
@@ -1,23 +1,23 @@
 import os
-from io import StringIO
+import StringIO
 import datetime
 
 
 def write_pubs():
-  s = StringIO()
+  s = StringIO.StringIO()
   header = "---\nlayout: page\ntitle: publications\npermalink: /pubs/\n---\n\n"
-  print (header, file=s)
+  print >>s, header
   update = '<h1>Publications</h1>\n<span class="post-date">(last update: %s)</span>' % (datetime.date.today().isoformat())
-  print (update, file=s)
+  print >>s, update
   toc = gettoc()
   warning = getwarning()
-  print (warning, file=s)
-  print (toc, file=s)
-  print ("\n{% raw %}", file=s)
+  print >>s, warning
+  print >>s, toc
+  print >>s, "\n{% raw %}"
 
   f = open('pubs.html')
-  print (f.read(), file=s)
-  print ('{% endraw %}', file=s)
+  print >>s, f.read()
+  print >>s, '{% endraw %}'
   os.remove('pubs.html')
   return s 
 
@@ -26,15 +26,9 @@ def getwarning():
     s = """<div class="message">
       I provide here the author's version of most of my papers. 
       These are for <em>personal use only</em>, and not for redistribution or commercial use. 
-      Please use the official published version ( <a><i class="fa fa-bookmark"></i></a> ) if you have access to it.
+      Please use the official published version ( <i class="fa fa-bookmark"></i> ) if you have access to it.
       <br><br>
-      <a><i class="ai ai-open-access-square"></i></a> = open access
-      <br><br>
-      <a><i class="fa fa-github"></i></a> = the "reproducibility repository", where the code and/or the data needed to reproduce the results of the paper, is given.
-      <br><br>
-      <i class="fa fa-external-link"></i> <a href='https://orcid.org/0000-0002-1251-8654'> my ORCID page</a>
-      <br>
-      <i class="fa fa-external-link"></i> <a href='http://www.scopus.com/inward/authorDetails.url?authorID=23988925500&partnerID=MN8TOARS'> my SCOPUS page</a>
+      Where applicable, a link to the "reproducibility repository" ( <i class="fa fa-github"></i> ), where the code and/or the data needed to reproduce the results of the paper, is given.
       </div>
     """
     return s
