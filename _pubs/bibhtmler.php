30ae2176f4b6b0a594a3b6735b9c16ce6c0040af
Hugo Ledoux <h.ledoux@tudelft.nl>
2019-09-30 08:38:16 +0200

    Field "info" for awards is made prettier

--- _pubs/bibhtmler.php
+++ _pubs/bibhtmler.php
@@ -483,8 +483,7 @@ class bibhtmler {
 			if (strlen($outnote) > 0) $out .= $outnote.". ";
 			
 			// if (array_key_exists('info', $in)) $out .= '<span class="label label-important">'.$in['info'].'</span>.';
-			if (array_key_exists('info', $in)) $out .= '<span class="label success">'.trim($in['info'], '{}').'</span>';
-			if (array_key_exists('note', $in)) $out .= '<span class="label warning">'.trim($in['note'], '{}').'</span>';
+			// if (array_key_exists('note', $in)) $out .= '<span class="label warning">'.trim($in['note'], '{}').'</span>';
 			// if (array_key_exists('oa', $in)) $out .= ' <a href="'.$this->processtext($in['doi']).'"><span class="label">open access</span></a>';
 			if (array_key_exists('oa', $in)) $out .= ' <a href="'.$this->processtext($in['doi']).'"><i class="ai ai-open-access-square"></i></a>';
 			// $out .= '<br>';
@@ -510,6 +509,7 @@ class bibhtmler {
 			if (array_key_exists('url', $in)) $out .= ' <a href="'.$this->processtext($in['url']).'"><i class="fa fa-external-link"></i></a>';
 			if (array_key_exists('doi', $in) and (array_key_exists('oa', $in) == FALSE) ) $out .= ' <a href="'.$this->processtext($in['doi']).'"><i class="fa fa-bookmark"></i></a>';
 			$out .= ' <a href="#bib'.$in['key'].'" data-toggle="collapse"><i class="fa fa-toggle-down"></i></a>';
+			if (array_key_exists('info', $in)) $out .= '<span class="label success">'.trim($in['info'], '{}').'</span>';
 			$out .= '<div id="bib'.$in['key'].'" class="collapse"  tabindex="-1"><pre class="bibtex">'.$this->getbibtex($in)."</pre></div>";
 		}
 	
