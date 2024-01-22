836675071b593c1ffc4a643646291f5d73c966f5
Hugo Ledoux <h.ledoux@tudelft.nl>
2024-01-22 14:39:37 +0100

    Fix url for server

--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,4 @@
 USERNAME=hledoux
 
 jekyll build
-rsync --delete -pthrvz ./_site/ ${USERNAME}@3d.bk.tudelft.nl:/var/www/people/hledoux/site/
+rsync --delete -pthrvz ./_site/ ${USERNAME}@3d:/var/www/people/hledoux/site/
